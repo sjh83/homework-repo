159f1337e3957b5334a19d5a0cf6ad55686b4a77
sjh83 <sjh908030@gmail.com>
2026-01-11 22:13:05 +0900

    완료

--- 2026-01-09/1-5-4/main.js
+++ 2026-01-09/1-5-4/main.js
@@ -1,18 +1,28 @@
 // 1. 계산 함수들 -> [math.js]로 이동 예정
-function add(a, b) {
+/* function add(a, b) {
   return a + b;
 }
 
 function sub(a, b) {
   return a - b;
 }
+ 이동 완료 */
 
+// import로 가져오기
+import { add, sub } from "./math.js";
 // 2. 프로필 데이터 -> [userProfile.js]로 이동 예정
+/*
+이동 완료 
 const userProfile = {
-  name: "홍길동",
-  age: 20,
+  name: "신진호",
+  age: 229,
 };
+*/
+// userProfile 대표 가져오기
+import User from "./userProfile.js";
 
 // 3. 실행 로직 (main.js에 유지)
 console.log(add(10, 20));
-console.log(userProfile.name);
+console.log(sub(20, 10));
+console.log(User.name);
+console.log(User.age);
---
+++ 2026-01-09/1-5-4/userProfile.js
@@ -0,0 +1,6 @@
+const userProfile = {
+  name: "신진호",
+  age: 229,
+};
+// 이 파일의 대표 1개만 가능
+export default userProfile;
