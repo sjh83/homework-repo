4d120ed090de0e6faed5d4b903a8bfd5ab53f9aa
sjh83 <sjh908030@gmail.com>
2026-01-11 21:45:55 +0900

    main.js

---
+++ 2026-01-09/1-5-4/main.js
@@ -0,0 +1,18 @@
+// 1. 계산 함수들 -> [math.js]로 이동 예정
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+// 2. 프로필 데이터 -> [userProfile.js]로 이동 예정
+const userProfile = {
+  name: "홍길동",
+  age: 20,
+};
+
+// 3. 실행 로직 (main.js에 유지)
+console.log(add(10, 20));
+console.log(userProfile.name);
